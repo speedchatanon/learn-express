7499edcdfbf7207a7072d39da159d352d5624b68
speedchatanon <87306469+speedchatanon@users.noreply.github.com>
2021-09-23 14:35:55 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const PORT = 9000;
 
 app.get("/", (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
